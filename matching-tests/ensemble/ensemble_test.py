20de53526b955f48170534ca539a9aa3600cd1a3
adambuttrick <adambuttrick@gmail.com>
2023-07-10 22:57:26 -0700

    Removing extraneous log stuff

--- matching-tests/ensemble/ensemble_test.py
+++ matching-tests/ensemble/ensemble_test.py
@@ -9,7 +9,7 @@ from predictor import Predictor
 PREDICTOR = Predictor('models/')
 now = datetime.now()
 script_start = now.strftime("%Y%m%d_%H%M%S")
-logging.basicConfig(filename=f'{script_start}_ensemble_test.log', level=logging.ERROR, format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(filename=f'{script_start}_ensemble_test.log', level=logging.ERROR)
 
 
 def query_affiliation(affiliation):
