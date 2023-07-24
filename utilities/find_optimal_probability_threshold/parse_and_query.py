8b60e937443debe4458853b69124f2ccb76af708
adambuttrick <adambuttrick@gmail.com>
2023-07-24 14:46:22 -0700

    Updating predictor to output None tuple and simplifying parse and query

--- utilities/find_optimal_probability_threshold/parse_and_query.py
+++ utilities/find_optimal_probability_threshold/parse_and_query.py
@@ -13,27 +13,26 @@ logging.basicConfig(filename=f'{script_start}_fasttext_test.log', level=logging.
 
 
 def parse_and_query(input_file, min_fasttext_probability):
-	results_set = []
-	try:
-		with open(input_file, 'r+', encoding='utf-8-sig') as f_in:
-			reader = csv.DictReader(f_in)
-			for row in reader:
-				affiliation = row['affiliation']
-				fasttext_prediction = PREDICTOR.predict_ror_id(
-					affiliation, min_fasttext_probability)
-				if fasttext_prediction:
-					predicted_ror_id, prediction_confidence = fasttext_prediction
-					if predicted_ror_id == row['ror_id']:
-						match = 'Y'
-					else:
-						match = 'N'
-				else:
-					match = 'NP'
-					predicted_ror_id, prediction_confidence = None, None
-				row["predicted_ror_id"] = predicted_ror_id
-				row["confidence"] = prediction_confidence
-				row["match"] = match
-				results_set.append(row)
-	except Exception as e:
-		logging.error(f'Error in parse_and_query: {e}')
-	return results_set
+    results_set = []
+    try:
+        with open(input_file, 'r+', encoding='utf-8-sig') as f_in:
+            reader = csv.DictReader(f_in)
+            for row in reader:
+                affiliation = row['affiliation']
+                fasttext_prediction = PREDICTOR.predict_ror_id(affiliation, min_fasttext_probability)
+                if fasttext_prediction:
+                    predicted_ror_id, prediction_confidence = fasttext_prediction
+                    match = 'Y' if predicted_ror_id == row['ror_id'] else 'N'
+                else:
+                    match, predicted_ror_id, prediction_confidence = 'NP', None, None
+                row.update({
+                    "predicted_ror_id": predicted_ror_id,
+                    "confidence": prediction_confidence,
+                    "match": match
+                })
+                results_set.append(row)
+    except Exception as e:
+        logging.error(f'Error in parse_and_query: {e}')
+
+    return results_set
+
