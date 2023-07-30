3547fd6a56a4bcc61356efa46247cdc04491ea97
adambuttrick <adambuttrick@gmail.com>
2023-07-29 17:49:21 -0700

    Updating ensemble test to be consistent with other scripts

--- matching-tests/ensemble/ensemble_test.py
+++ matching-tests/ensemble/ensemble_test.py
@@ -12,65 +12,63 @@ script_start = now.strftime("%Y%m%d_%H%M%S")
 logging.basicConfig(filename=f'{script_start}_ensemble_test.log', level=logging.ERROR, format='%(asctime)s %(levelname)s %(message)s')
 
 
+import requests
+from urllib.parse import quote
+
 def query_affiliation(affiliation):
-    chosen_id = None
-    affiliation_encoded = quote(affiliation)
-    url = f"https://api.ror.org/organizations?affiliation={affiliation_encoded}"
-    r = requests.get(url)
-    if r.ok:
+    try:
+        chosen_id = None
+        score = None
+        url = "https://api.ror.org/organizations"
+        params = {"affiliation": affiliation}
+        r = requests.get(url, params=params)
         api_response = r.json()
         results = api_response['items']
         if results:
             for result in results:
                 if result['chosen']:
                     chosen_id = result['organization']['id']
-    return chosen_id
+                    score = result['score']
+    except Exception as e:
+        logging.error(f'Error for query: {affiliation} - {e}')
+    return chosen_id, score
 
 
-def ensemble_match(affiliation, confidence=0.8, match_order='fasttext'):
+def ensemble_match(affiliation, min_fasttext_probability=0.8, match_order='fasttext'):
     if match_order == 'fasttext':
-        fasttext_prediction = PREDICTOR.predict_ror_id(affiliation, confidence)
-        if fasttext_prediction[0] is not None:
-            return fasttext_prediction
+        fasttext_ror_id, fasttext_probability = PREDICTOR.predict_ror_id(affiliation, min_fasttext_probability)
+        if fasttext_ror_id:
+            return fasttext_ror_id, fasttext_probability
         else:
-            affiliation_prediction = query_affiliation(affiliation)
-            if affiliation_prediction is not None:
-                return affiliation_prediction, None
+            ror_aff_predicted_id, ror_aff_score = query_affiliation(affiliation)
+            return ror_aff_predicted_id, ror_aff_score
     else:
-        affiliation_prediction = query_affiliation(affiliation)
-        if affiliation_prediction is not None:
-            return affiliation_prediction, None
+        ror_aff_predicted_id, ror_aff_score = query_affiliation(affiliation)
+        if ror_aff_predicted_id:
+            return ror_aff_predicted_id, ror_aff_score
         else:
-            fasttext_prediction = PREDICTOR.predict_ror_id(
-                affiliation, confidence)
-            if fasttext_prediction[0] is not None:
-                return fasttext_prediction
+            fasttext_ror_id, fasttext_probability = PREDICTOR.predict_ror_id(affiliation, min_fasttext_probability)
+            return fasttext_ror_id, fasttext_probability
 
 
-def parse_and_query(input_file, output_file, confidence, match_order):
+def parse_and_query(input_file, output_file, min_fasttext_probability, match_order):
     try:
         with open(input_file, 'r+', encoding='utf-8-sig') as f_in:
             reader = csv.DictReader(f_in)
+            fieldnames = reader.fieldnames + ["prediction", "probability", "match"]
             with open(output_file, 'w') as f_out:
-                writer = csv.writer(f_out)
-                writer.writerow(reader.fieldnames +
-                                ["prediction", "confidence", "match"])
-            for row in reader:
-                affiliation = row['affiliation']
-                prediction = ensemble_match(
-                    affiliation, confidence, match_order)
-                predicted_id, prediction_confidence = prediction[0], prediction[1]
-                if predicted_id:
-                    if predicted_id == row['ror_id']:
-                        match = 'Y'
-                    else:
-                        match = 'N'
-                else:
-                    match = 'NP'
-                with open(output_file, 'a') as f_out:
-                    writer = csv.writer(f_out)
-                    writer.writerow(list(row.values()) +
-                                    [predicted_id, prediction_confidence, match])
+                writer = csv.DictWriter(f_out, fieldnames=fieldnames)
+                writer.writeheader()
+                for row in reader:
+                    affiliation = row['affiliation']
+                    predicted_id, prediction_score = ensemble_match(affiliation, min_fasttext_probability, match_order)
+                    match = 'Y' if predicted_id and predicted_id == row['ror_id'] else ('NP' if not predicted_id else 'N')
+                    row.update({
+                        "prediction": predicted_id,
+                        "probability": prediction_score,
+                        "match": match
+                    })
+                    writer.writerow(row)
     except Exception as e:
         logging.error(f'Error in parse_and_query: {e}')
 
@@ -82,7 +80,7 @@ def parse_arguments():
     parser.add_argument('-o', '--output', help='Output CSV file',
                         default='ensemble_results.csv')
     parser.add_argument(
-        '-c', '--confidence', help='Confidence level for the fasttext predictor', type=float, default=0.8)
+        '-c', '--min_fasttext_probability', help='min_fasttext_probability level for the fasttext predictor', type=float, default=0.8)
     parser.add_argument('-m', '--match_order',
                         help='Order of matching methods ("fasttext" or "affiliation")', type=str, default='fasttext')
     return parser.parse_args()
@@ -90,7 +88,7 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    parse_and_query(args.input, args.output, args.confidence, args.match_order)
+    parse_and_query(args.input, args.output, args.min_fasttext_probability, args.match_order)
 
 
 if __name__ == '__main__':
