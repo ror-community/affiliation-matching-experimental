443edddd5e4bbb279c50316253f4efbae20bbdc5
adambuttrick <adambuttrick@gmail.com>
2023-08-14 00:17:11 -0700

    initial commit

--- utilities/ner_indexes/deduplicate_and_remove_short.py
+++ utilities/ner_indexes/deduplicate_and_remove_short.py
@@ -7,8 +7,8 @@ import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Filter NER index CSV to remove duplicates and short strings")
-    parser.add_argument("input_file", help="Input CSV file")
-    parser.add_argument("output_file", help="Output CSV file")
+    parser.add_argument("-i", "--input", help="Input CSV file")
+    parser.add_argument("-o","--output", help="Output CSV file")
     parser.add_argument("-l", "--length", type=int, default=5,
                         help="Minimum length of substring to keep (default: 5)")
     return parser.parse_args()
@@ -16,20 +16,23 @@ def parse_arguments():
 
 def filter_csv(input_file, output_file, min_length):
     seen_rows = set()
-    with open(input_file, 'r') as inp, open(output_file, 'w', newline='') as out:
-        reader = csv.DictReader(inp)
-        writer = csv.DictWriter(out, fieldnames=reader.fieldnames)
+    with open(input_file, 'r') as f_in, open(output_file, 'w', newline='') as f_out:
+        reader = csv.DictReader(f_in)
+        writer = csv.DictWriter(f_out, fieldnames=reader.fieldnames)
         writer.writeheader()
         for row in reader:
             row_hash = hashlib.sha1(str(row).encode()).hexdigest()
-            if len(row['index_substring']) > min_length and row_hash not in seen_rows:
+            if row['entity_type'] == 'name' and len(row['index_substring']) > min_length and row_hash not in seen_rows:
+                writer.writerow(row)
+                seen_rows.add(row_hash)
+            elif row['entity_type'] == 'location' and row_hash not in seen_rows:
                 writer.writerow(row)
                 seen_rows.add(row_hash)
 
 
 def main():
     args = parse_arguments()
-    filter_csv(args.input_file, args.output_file, args.length)
+    filter_csv(args.input, args.output, args.length)
 
 
 if __name__ == "__main__":
